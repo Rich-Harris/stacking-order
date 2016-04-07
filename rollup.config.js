33cde3075705f7a84508a53eb0f10a952ece6b67
Rich Harris <richard.a.harris@gmail.com>
2016-04-07 18:23:32 -0400

    use buble

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,8 @@
-import babel from 'rollup-plugin-babel';
+import buble from 'rollup-plugin-buble';
 
 export default {
 	entry: 'src/index.js',
-	plugins: [ babel() ],
+	plugins: [ buble() ],
 	moduleName: 'stackingOrder',
 	sourceMap: true
 };
